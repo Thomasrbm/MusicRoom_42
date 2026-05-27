17faedba470d48d77cab8bf710499b1d0d9476a8
rbardet <robin.bardet.tomczak@gmail.com>
2026-05-27 19:09:35 +0200

    Feat: add drizzle command to package.json

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "drizzle-kit";
 // out un binaire pour drizzle
 
 export default defineConfig({
-  schema: "./schema.ts",
-  out: "./drizzle",
+  schema: "./database/schema.ts",
+  out: "./database/drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
